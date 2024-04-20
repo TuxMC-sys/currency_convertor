57740ae5f0634246ce7664bf1636d557affa7389
TuxMC-sys <109832568+TuxMC-sys@users.noreply.github.com>
2024-04-20 17:48:57 -0400

    Dealing with compiler errors, some of the logic is sound but, I need to get the file saving stuff right.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use std::collections::HashMap;
 use std::io;
+use std::fs;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+use dirs::home_dir;
 use reqwest::header::{HeaderMap, HeaderName, HeaderValue};
 use reqwest::{Client};
-use serde::Deserialize;
-#[derive(Deserialize)]
+use serde::{Deserialize, Serialize};
+use serde_json::{json, to_vec};
+
+#[derive(Deserialize, Serialize)]
 struct ApiReturn {
     disclaimer: String,
     license: String,
@@ -13,11 +20,17 @@ struct ApiReturn {
 }
 #[tokio::main]
 async fn main() {
+    println!("Do you need to update your exchange rates? Type y for yes, else click enter.");
+    let mut update_var = String::new();
+    io::stdin().read_line(&mut update_var).unwrap();
+    if update_var == "y"{
+        save_currencies(request().await);
+    }
     let request = request().await;
     println!("{:#?}",request.rates);
 }
 async fn request() -> ApiReturn {
-    let app_id = input();
+    let app_id = app_id();
     let url = format!("https://openexchangerates.org/api/latest.json?app_id={app_id}&base=USD&prettyprint=true&show_alternative=true");
     let mut headers = HeaderMap::new();
     headers.insert(HeaderName::from_static("accept"), HeaderValue::from_static("application/json"));
@@ -26,15 +39,36 @@ async fn request() -> ApiReturn {
         .await.unwrap().text().await.unwrap();
     serde_json::from_str(response.as_str()).unwrap()
 }
-fn input() -> String{
-    println!("Do you need to update your exchange rate? Type y for yes, else click enter.");
-    let mut update_var = String::new();
-    io::stdin().read_line(&mut update_var).unwrap();
-    if update_var == "y"{
-        //tbd
+fn app_id() -> String{
+    let app_id_string = fs::read_to_string(config_file().as_mut_os_string().append("app_id"));
+    match app_id_string {
+        Ok(app_id_string) => app_id_string,
+        _Err => {
+            let mut app_id = String::new();
+            println!("Input your openexchangerates.org app id.");
+            io::stdin().read_line(&mut app_id).unwrap();
+            let mut file = File::create(config_file()).unwrap();
+            file.write(app_id.as_bytes()).unwrap();
+            app_id
+        }
     }
-    let mut app_id = String::new();
-    println!("Input your openexchangerates.org app id.");
-    io::stdin().read_line(&mut app_id).unwrap();
-    app_id
+}
+fn save_currencies(save_json: ApiReturn){
+    fs::write(
+        config_file().push("currency.json").as_path(),
+        json![to_vec(&save_json)]
+    )
+        .expect("Should create or overwrite file.");
+}
+fn config_file() -> PathBuf {
+    PathBuf::from(
+        &[
+            home_dir()
+                .unwrap_or_else(|| "".into())
+                .display()
+                .to_string(),
+            "/Documents/".to_string(),
+        ]
+            .join(""),
+    )
 }
