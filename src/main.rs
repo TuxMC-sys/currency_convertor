b4e60b03892b31626515d68ae5bdd70ed8c52f87
Linux Rustling <109832568+TuxMC-sys@users.noreply.github.com>
2024-06-15 18:28:10 -0400

    Updated core functionallity to include clap and reorganized code. QOL improvments.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
-use std::{collections::HashMap, path::PathBuf, io, io::Write, fs, fs::File};
 use dirs::home_dir;
-use reqwest::{Client, header::{HeaderMap, HeaderName, HeaderValue}};
+use reqwest::{
+    header::{HeaderMap, HeaderName, HeaderValue},
+    Client,
+};
 use serde::{Deserialize, Serialize};
 use serde_json::to_vec;
+use std::{collections::HashMap, fs, fs::File, io, io::Write, path::PathBuf};
+use clap::Parser;
+#[derive(Parser)]
+#[command(version, about, long_about = "This program allows you to convert currencies using the openexchangerates.org API.")]
+struct Cli {
+    amount: f32,
+    orgin_currency: String,
+    final_currency: String,
+    #[arg(short, long, help="Gets latest exchange rates using your API key.")]
+    refresh: bool,
+}
 #[derive(Deserialize, Serialize)]
 struct ApiReturn {
     disclaimer: String,
     license: String,
     timestamp: f32,
     base: String,
-    rates: HashMap<String, f32>
+    rates: HashMap<String, f32>,
 }
 #[tokio::main]
 async fn main() {
-    println!("Do you need to update your exchange rates? Type y for yes, else click enter.");
-    let mut update_var = String::new();
-    io::stdin().read_line(&mut update_var).unwrap();
-    if update_var == "y\n"{
+    let cli = Cli::parse();
+    if cli.refresh{
         save_currencies(request_rates().await);
     }
-    convert_currencies(load_currencies().rates);
+    convert_currencies(load_currencies().rates, cli);
 }
 async fn request_rates() -> ApiReturn {
     let app_id = app_id();
@@ -27,13 +38,23 @@ async fn request_rates() -> ApiReturn {
         "https://openexchangerates.org/api/latest.json?app_id={app_id}&base=USD&prettyprint=true&show_alternative=true"
     );
     let mut headers = HeaderMap::new();
-    headers.insert(HeaderName::from_static("accept"), HeaderValue::from_static("application/json"));
+    headers.insert(
+        HeaderName::from_static("accept"),
+        HeaderValue::from_static("application/json"),
+    );
     let client = Client::new();
-    let response = client.get(url).headers(headers).send()
-        .await.unwrap().text().await.unwrap();
+    let response = client
+        .get(url)
+        .headers(headers)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
     serde_json::from_str(response.as_str()).unwrap()
 }
-fn app_id() -> String{
+fn app_id() -> String {
     let mut path = config_file().into_os_string();
     path.push("app_id");
     let app_id_string = fs::read_to_string(path.as_os_str());
@@ -49,35 +70,29 @@ fn app_id() -> String{
         }
     }
 }
-fn save_currencies(save_json: ApiReturn){
+fn save_currencies(save_json: ApiReturn) {
     let mut path = config_file().into_os_string();
     path.push("currency.json");
     fs::write(path.as_os_str(), to_vec(&save_json).unwrap())
         .expect("Should create or overwrite file.");
 }
-fn load_currencies() -> ApiReturn{
+fn load_currencies() -> ApiReturn {
     let mut path = config_file().into_os_string();
     path.push("currency.json");
     serde_json::from_slice(&fs::read(path).expect("Re-run and refresh your currencies.")).unwrap()
 }
-fn convert_currencies(currency_map: HashMap<String, f32>){
-    println!("What currency do you want to convert from (i.e. USD, GBP, or EUR)?");
-    let mut orgin_currency = String::new();
-    io::stdin().read_line(&mut orgin_currency).unwrap();
-    orgin_currency.truncate(orgin_currency.len() - 1);
-    println!("What currency do you want to convert to?");
-    let mut final_currency = String::new();
-    io::stdin().read_line(&mut final_currency).unwrap();
-    final_currency.truncate(final_currency.len() - 1);
-    println!("How much {} do you want to know the value of in {}?", orgin_currency, final_currency);
-    let mut amount = String::new();
-    io::stdin().read_line(&mut amount).unwrap();
-    let orgin_multiplier = currency_map.get(&orgin_currency).expect("Invalid currency name");
-    let final_multiplier = currency_map.get(&final_currency).expect("Invalid currency name");
-    amount.truncate(amount.len()-1);
-    let amount = amount.parse::<f32>().unwrap();
-    let final_amount = orgin_multiplier * final_multiplier * amount;
-    println!("{} {} is {} {}",amount, orgin_currency, final_amount, final_currency)
+fn convert_currencies(currency_map: HashMap<String, f32>, arguments: Cli) {
+    let orgin_multiplier = currency_map
+        .get(&arguments.orgin_currency)
+        .expect("Invalid currency name");
+    let final_multiplier = currency_map
+        .get(&arguments.final_currency)
+        .expect("Invalid currency name");
+    let final_amount = orgin_multiplier * final_multiplier * arguments.amount;
+    println!(
+        "{} {} is {} {}",
+        arguments.amount, arguments.orgin_currency, final_amount, arguments.final_currency
+    )
 }
 
 fn config_file() -> PathBuf {
@@ -89,6 +104,6 @@ fn config_file() -> PathBuf {
                 .to_string(),
             "/Documents/".to_string(),
         ]
-            .join(""),
+        .join(""),
     )
 }
